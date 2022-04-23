fd14f8e84a28144829ba5637de87ab9b98d6d5fd
saud-py <57790931+saud-py@users.noreply.github.com>
2022-04-23 19:26:39 +0530

    Online Food Ordering Management System

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- footer Section Starts-->
+<div class= "footer">
+            <div class = "wrapper">
+                <p class = "text-center">Developed by -<a href="#"> Saud ali and Saba Fathima</a> </p>
+            <div>
+        </div>
+        <!-- footert Section Ends-->
+
+    </body>
+</html>
